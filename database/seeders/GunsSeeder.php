8243f5279efa961c5ac361ccb6bb874b2d178170
karlos1998 <fadeusz98@gmail.com>
2026-01-21 00:29:34 +0100

    update guns seeder with detailed descriptions for each firearm to enhance data richness and clarity

--- database/seeders/GunsSeeder.php
+++ database/seeders/GunsSeeder.php
@@ -59,41 +59,171 @@ class GunsSeeder extends Seeder
         }
 
         $guns = [
-            ['name' => 'CZ 75 Shadow 1', 'type' => 'Pistolet', 'caliber' => '9x19'],
-            ['name' => 'CZ P-09', 'type' => 'Pistolet', 'caliber' => '9x19'],
-            ['name' => 'CZ P-10 F', 'type' => 'Pistolet', 'caliber' => '9x19'],
-            ['name' => 'CZ 75 Compact', 'type' => 'Pistolet', 'caliber' => '9x19'],
+            [
+                'name' => 'CZ 75 Shadow 1 (SP-01 Shadow)',
+                'type' => 'Pistolet',
+                'caliber' => '9x19',
+                'description' => 'Pełnowymiarowy pistolet SA/DA oparty na platformie CZ 75 SP-01, popularny w strzelectwie dynamicznym. Łączy klasyczną konstrukcję CZ z udoskonaloną ergonomią.',
+            ],
+            [
+                'name' => 'CZ P-09',
+                'type' => 'Pistolet',
+                'caliber' => '9x19',
+                'description' => 'Pełnowymiarowy pistolet SA/DA z polimerowym szkieletem wzmacnianym włóknem szklanym. Ma szynę akcesoryjną i wymienne nakładki chwytu.',
+            ],
+            [
+                'name' => 'CZ P-10 F',
+                'type' => 'Pistolet',
+                'caliber' => '9x19',
+                'description' => 'Pełnowymiarowy pistolet bijnikowy (striker-fired) z rodziny P-10. Wersja F oferuje powiększoną pojemność magazynka i dłuższą linię celowania.',
+            ],
+            [
+                'name' => 'CZ 75 Compact',
+                'type' => 'Pistolet',
+                'caliber' => '9x19',
+                'description' => 'Kompaktowa wersja klasycznej CZ 75 ze stalowym szkieletem. Zachowuje charakterystykę strzelania serii CZ 75 w mniejszych gabarytach.',
+            ],
 
-            ['name' => 'Ruger GP100 (3")', 'type' => 'Rewolwer', 'caliber' => '.357 Magnum'],
+            [
+                'name' => 'Ruger GP100 (3")',
+                'type' => 'Rewolwer',
+                'caliber' => '.357 Magnum',
+                'description' => 'Rewolwer double-action Ruger GP100 w kalibrze .357 Magnum. Seria GP100 jest oferowana w różnych konfiguracjach lufy i pojemności bębna.',
+            ],
 
-            ['name' => 'Mossberg 500', 'type' => 'Strzelba', 'caliber' => '12/70'],
-            ['name' => 'Baikal IZH-27 / MP-27', 'type' => 'Strzelba', 'caliber' => '12/70'],
+            [
+                'name' => 'Mossberg 500',
+                'type' => 'Strzelba',
+                'caliber' => '12/70',
+                'description' => 'Seria strzelb powtarzalnych (pump-action) produkowana przez O.F. Mossberg & Sons od 1961 roku. Występuje w wielu wariantach konfiguracji i przeznaczenia.',
+            ],
+            [
+                'name' => 'Baikal IZH-27 / MP-27',
+                'type' => 'Strzelba',
+                'caliber' => '12/70',
+                'description' => 'Dwulufowa strzelba łamana typu over-and-under (nadlufka) z zakładów w Iżewsku. Model produkowany jest seryjnie od lat 70. i występuje w kilku kalibrach.',
+            ],
 
-            ['name' => 'WBP Jack 556', 'type' => 'Karabin', 'caliber' => '5.56x45 / .223 Rem'],
-            ['name' => 'BURGU BRG55 (AR-15)', 'type' => 'Karabin', 'caliber' => '5.56x45 / .223 Rem'],
-            ['name' => 'Diamondback DB15 (AR-15)', 'type' => 'Karabin', 'caliber' => '5.56x45 / .223 Rem'],
+            [
+                'name' => 'WBP Jack 556',
+                'type' => 'Karabin',
+                'caliber' => '5.56x45 / .223 Rem',
+                'description' => 'Samopowtarzalny karabinek w stylu AK, produkowany w Polsce przez WBP i oferowany m.in. w 5.56×45 /.223 Rem. Seria Jack to odmiany karabinków w układzie AKM.',
+            ],
+            [
+                'name' => 'BURGU BRG55 (AR-15)',
+                'type' => 'Karabin',
+                'caliber' => '5.56x45 / .223 Rem',
+                'description' => 'Karabinek oparty o platformę AR-15 w kalibrze 5.56×45 /.223 Rem. W materiałach produktowych często podkreślany jest układ gazowy z tłokiem o krótkim skoku.',
+            ],
+            [
+                'name' => 'Diamondback DB15 (AR-15)',
+                'type' => 'Karabin',
+                'caliber' => '5.56x45 / .223 Rem',
+                'description' => 'Rodzina karabinków DB15 na platformie AR-15, oferowana m.in. w 5.56 NATO. Występuje w wielu wariantach (różne lufy, łoża i konfiguracje).',
+            ],
 
-            ['name' => 'WBP Mini Jack (7.62x39)', 'type' => 'Karabin', 'caliber' => '7.62x39'],
-            ['name' => 'Mosin-Nagant (np. 91/30)', 'type' => 'Karabin', 'caliber' => '7.62x54R'],
+            [
+                'name' => 'WBP Mini Jack (7.62x39)',
+                'type' => 'Karabin',
+                'caliber' => '7.62x39',
+                'description' => 'Samopowtarzalny subkarabinek w kalibrze 7.62×39 produkowany przez WBP. Jest oparty o konstrukcję AKM i wyróżnia się skróconą konfiguracją.',
+            ],
+            [
+                'name' => 'Mosin-Nagant (np. 91/30)',
+                'type' => 'Karabin',
+                'caliber' => '7.62x54R',
+                'description' => 'Historyczny karabin powtarzalny (bolt-action) z integralnym magazynkiem na 5 nabojów. Standardowo kojarzony z nabojem 7.62×54R i masową produkcją wojskową.',
+            ],
 
-            ['name' => 'Ruger Mark IV 22/45', 'type' => 'Pistolet', 'caliber' => '.22 LR'],
-            ['name' => 'ALFA Steel (revolver)', 'type' => 'Rewolwer', 'caliber' => '.357 Magnum'],
-            ['name' => 'CZ 457', 'type' => 'Karabin', 'caliber' => '.22 LR'],
-            ['name' => 'CZ 455', 'type' => 'Karabin', 'caliber' => '.22 LR'],
-            ['name' => 'Hammerli TAC R1 (.22 LR)', 'type' => 'Karabin', 'caliber' => '.22 LR'],
+            [
+                'name' => 'Ruger Mark IV 22/45',
+                'type' => 'Pistolet',
+                'caliber' => '.22 LR',
+                'description' => 'Pistolet bocznego zapłonu w kalibrze .22 LR z serii Ruger Mark IV. Wariant 22/45 nawiązuje ergonomią do pistoletów centralnego zapłonu.',
+            ],
+            [
+                'name' => 'ALFA Steel (revolver)',
+                'type' => 'Rewolwer',
+                'caliber' => '.357 Magnum',
+                'description' => 'Stalowy rewolwer Alfa-Proj w kalibrze .357 Magnum (często także z obsługą .38 Special). Spotykany w wersjach z regulowanymi przyrządami celowniczymi.',
+            ],
+            [
+                'name' => 'CZ 457',
+                'type' => 'Karabin',
+                'caliber' => '.22 LR',
+                'description' => 'Seria karabinów bocznego zapłonu CZ 457 o modułowej konstrukcji. Producent wskazuje na łatwą personalizację i możliwość konwersji w obrębie rodziny (.22 LR / .17 HMR / .22 WMR).',
+            ],
+            [
+                'name' => 'CZ 455',
+                'type' => 'Karabin',
+                'caliber' => '.22 LR',
+                'description' => 'Magazynkowy karabin powtarzalny (bolt-action) bocznego zapłonu z rodziny CZ 455. Seria była produkowana w latach 2010–2018 i występowała w kilku kalibrach rimfire.',
+            ],
+            [
+                'name' => 'Hämmerli TAC R1 (.22 LR)',
+                'type' => 'Karabin',
+                'caliber' => '.22 LR',
+                'description' => 'Samopowtarzalny karabinek treningowo-sportowy w kalibrze .22 LR o “taktycznej” ergonomii. Producent akcentuje m.in. łoże M-LOK i składane przyrządy celownicze.',
+            ],
 
-            ['name' => 'Celik Crossline (9x19)', 'type' => 'Karabin', 'caliber' => '9x19'],
-            ['name' => 'Springfield Echelon', 'type' => 'Pistolet', 'caliber' => '9x19'],
-            ['name' => 'Ruger PC Carbine', 'type' => 'Karabin', 'caliber' => '9x19'],
-            ['name' => 'Glock 19', 'type' => 'Pistolet', 'caliber' => '9x19'],
-            ['name' => 'Canik SFx Rival-S', 'type' => 'Pistolet', 'caliber' => '9x19'],
+            [
+                'name' => 'Celik Crossline (9x19)',
+                'type' => 'Karabin',
+                'caliber' => '9x19',
+                'description' => 'Karabinek w kalibrze 9×19, oferowany m.in. w długościach luf 8" i 16". Opisy produktu wskazują na rozwiązania sterowania inspirowane AR-15 i elementy obustronne.',
+            ],
+            [
+                'name' => 'Springfield Echelon',
+                'type' => 'Pistolet',
+                'caliber' => '9x19',
+                'description' => 'Modułowy pistolet bijnikowy 9×19 z wymiennym stalowym “chassis” (COG) i systemem montażu optyki. Linia została wprowadzona w 2023 roku.',
+            ],
+            [
+                'name' => 'Ruger PC Carbine',
+                'type' => 'Karabin',
+                'caliber' => '9x19',
+                'description' => 'Samopowtarzalny karabinek na amunicję pistoletową 9×19, z prostym odrzutem zamka (blowback). Nowa odsłona PC Carbine wróciła do produkcji jako wersja “takedown” od 2017 roku.',
+            ],
+            [
+                'name' => 'Glock 19',
+                'type' => 'Pistolet',
+                'caliber' => '9x19',
+                'description' => 'Kompaktowy pistolet 9 mm Luger, projektowany jako uniwersalny “all-round”. Producent wskazuje na zredukowane wymiary względem pełnowymiarowych modeli.',
+            ],
+            [
+                'name' => 'Canik SFx Rival-S',
+                'type' => 'Pistolet',
+                'caliber' => '9x19',
+                'description' => 'Pistolet bijnikowy z metalowym (stalowym) szkieletem, kierowany pod strzelectwo sportowe. Producent podkreśla stalową ramę i rozwiązania pod dynamikę.',
+            ],
 
-            ['name' => 'HK SP5 (MP5)', 'type' => 'Karabin', 'caliber' => '9x19'],
-            ['name' => 'CZ Scorpion EVO 3', 'type' => 'Karabin', 'caliber' => '9x19'],
+            [
+                'name' => 'HK SP5',
+                'type' => 'Karabin',
+                'caliber' => '9x19',
+                'description' => 'Półautomatyczna, cywilna wersja legendarnego MP5 w kalibrze 9×19. Wyróżnia ją mechanizm opóźnionego odrzutu rolkowego (roller-delayed blowback).',
+            ],
+            [
+                'name' => 'CZ Scorpion EVO 3 S1',
+                'type' => 'Karabin',
+                'caliber' => '9x19',
+                'description' => 'Platforma 9×19 produkowana przez CZ, dostępna m.in. jako wersja cywilna S1 (carbine/pistol). Opisy producenta wskazują na ambi sterowanie i szerokie możliwości doposażenia.',
+            ],
 
-            ['name' => 'Rossi Puma (.357)', 'type' => 'Karabin', 'caliber' => '.357 Magnum'],
+            [
+                'name' => 'Rossi Puma (.357)',
+                'type' => 'Karabin',
+                'caliber' => '.357 Magnum',
+                'description' => 'Klasyczny karabinek lever-action w kalibrze .357 Magnum / .38 Special. Opisy rynkowe wskazują inspirację konstrukcjami typu Winchester 1892 i typowe przyrządy “buckhorn”.',
+            ],
 
-            ['name' => 'FX-9 (9mm)', 'type' => 'Karabin', 'caliber' => '9x19'],
+            [
+                'name' => 'Freedom Ordnance FX-9 (9mm)',
+                'type' => 'Karabin',
+                'caliber' => '9x19',
+                'description' => 'Modułowa platforma 9 mm projektowana pod szeroką kompatybilność z akcesoriami w standardzie AR. Producent podkreśla modularność i łatwe konfigurowanie wariantów.',
+            ],
         ];
 
         foreach ($guns as $gun) {
@@ -105,7 +235,7 @@ class GunsSeeder extends Seeder
                 [
                     'gun_type_id' => $type->id,
                     'caliber_id' => $caliber->id,
-                    'description' => null,
+                    'description' => $gun['description'],
                     'photos' => [],
                 ],
             );
